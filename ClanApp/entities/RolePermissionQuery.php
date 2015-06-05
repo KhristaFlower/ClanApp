31cb4500b52fc932417e1f179397117dc985b255
Christopher Sharman <christopher.p.sharman@gmail.com>
2015-06-05 23:07:07 +0000

    Generated entities for the current schema.

---
+++ ClanApp/entities/RolePermissionQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ClanApp\entities;
+
+use ClanApp\entities\Base\RolePermissionQuery as BaseRolePermissionQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'role_permissions' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class RolePermissionQuery extends BaseRolePermissionQuery
+{
+
+}
