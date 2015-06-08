e78b7ea608b02a325c35df1254e8e919f0f800f5
Christopher Sharman <christopher.p.sharman@gmail.com>
2015-06-08 20:59:41 +0000

    dieDumpJSON - A nice quick json dumper used to quickly see a human readable output of variablees.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,23 @@
 <?php
 
+define('PROJECT_ROOT', __DIR__ . '/../');
+
+function dieDumpJSON(...$arguments) {
+
+	$output = [];
+
+	$argNo = 0;
+
+	foreach ($arguments as $arg) {
+		$output["Arg$argNo"] = $arg;
+		$argNo++;
+	}
+
+	header('content-type:text/json');
+	exit(json_encode($output));
+
+}
+
 require_once '../vendor/autoload.php';
 
 $whoops = new \Whoops\Run();
