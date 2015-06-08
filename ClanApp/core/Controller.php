059acc26c836cebc09bf071390b96510bfe99647
Christopher Sharman <christopher.p.sharman@gmail.com>
2015-06-08 23:49:52 +0000

    Whitespace changes.

--- ClanApp/core/Controller.php
+++ ClanApp/core/Controller.php
@@ -4,15 +4,15 @@ namespace ClanApp\core;
 
 class Controller {
 
-    /**
-     * @var Router
-     */
-    private $router;
+	/**
+	 * @var Router
+	 */
+	private $router;
 
-    /**
-     * @var \Smarty
-     */
-    private $smarty;
+	/**
+	 * @var \Smarty
+	 */
+	private $smarty;
 
 	/**
 	 * @var Sidebar
@@ -24,21 +24,21 @@ class Controller {
 	 */
 	private $authenticator;
 
-    public function __construct() {
+	public function __construct() {
 
-        $this->smarty = new \Smarty();
-        $this->smarty->setTemplateDir($_SERVER['DOCUMENT_ROOT'] . "/../ClanApp/templates/");
-        $this->smarty->setCompileDir($_SERVER['DOCUMENT_ROOT'] . "/../ClanApp/data/templates_c/");
-        $this->smarty->setConfigDir($_SERVER['DOCUMENT_ROOT'] . "/../ClanApp/configs/");
-        $this->smarty->setCacheDir($_SERVER['DOCUMENT_ROOT'] . "/../ClanApp/cache/");
+		$this->smarty = new \Smarty();
+		$this->smarty->setTemplateDir($_SERVER['DOCUMENT_ROOT'] . "/../ClanApp/templates/");
+		$this->smarty->setCompileDir($_SERVER['DOCUMENT_ROOT'] . "/../ClanApp/data/templates_c/");
+		$this->smarty->setConfigDir($_SERVER['DOCUMENT_ROOT'] . "/../ClanApp/configs/");
+		$this->smarty->setCacheDir($_SERVER['DOCUMENT_ROOT'] . "/../ClanApp/cache/");
 		$this->smarty->force_compile = true;
-        $this->smarty->debugging = false;
+		$this->smarty->debugging = false;
 
 		$this->authenticator = new Authenticator();
 
 		$this->generateSidebar();
 
-    }
+	}
 
 	/*
 	 * Fired after construct and before the requested action.
@@ -47,21 +47,21 @@ class Controller {
 
 	}
 
-    public function setRouter(Router $router) {
-        $this->router = $router;
-    }
+	public function setRouter(Router $router) {
+		$this->router = $router;
+	}
 
-    public function renderView() {
+	public function renderView() {
 
 		if ($this->sidebar) {
 			$this->smarty->assign('sidebar', $this->sidebar->getStructure());
 		}
 
-        $controllerName = $this->router->getControllerName();
-        $actionName = $this->router->getActionName();
-        $displayFile = "pages/$controllerName/$actionName.tpl";
-        $this->smarty->display($displayFile);
-    }
+		$controllerName = $this->router->getControllerName();
+		$actionName = $this->router->getActionName();
+		$displayFile = "pages/$controllerName/$actionName.tpl";
+		$this->smarty->display($displayFile);
+	}
 
 	public function generateSidebar() {
 
