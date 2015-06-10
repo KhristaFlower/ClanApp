9a1bbd194a4df1c401d68cdb1ee1f4d4127d4b70
Christopher Sharman <christopher.p.sharman@gmail.com>
2015-06-10 22:48:31 +0000

    Added two new controllers for testing a new route parser.

--- ClanApp/controllers/ManageController.php
+++ ClanApp/controllers/ManageController.php
@@ -8,11 +8,13 @@ class ManageController extends Controller {
 
 	public function __construct() {
 
-
+		parent::__construct();
 
 	}
 
 	public function generateSidebar() {
+
+		parent::generateSidebar();
 
 		$this->sidebar->addList('', [
 			'/manage/members' => ''
---
+++ ClanApp/controllers/manage/MembersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ClanApp\controllers\manage;
+
+use ClanApp\core\Controller;
+
+class MembersController extends Controller{
+
+	public function view() {
+
+	}
+
+}
